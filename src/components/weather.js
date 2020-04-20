3b39fdc2ac9c20e649df4e95e4eb81e5aedb8a5b
Yevhenii <oleshkoevgeniy@gmail.com>
2020-04-20 22:47:13 +0300

    fix temp

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,10 +4,10 @@ const Weather = props => (
   <div>
     { props.city &&
       <div>
-        <p>Местоположение: {props.city}, {props.country}</p>
-        <p>Температура: props.temp}</p>
-        <p>Давление: {props.pressure}</p>
-        <p>Заход солнца: {props.sunset}</p>
+        <p>Местоположение: { props.city }, { props.country }</p>
+        <p>Температура: { props.temp }</p>
+        <p>Давление: { props.pressure }</p>
+        <p>Заход солнца: { props.sunset }</p>
       </div>
     }
 
